62bc575fb1c7eed0ad908f1f72e725b7f4a577aa
Jan Vlnas <jan.vlnas@superface.ai>
2022-02-21 16:03:27 +0100

    feat(tokens): Handle automatic token refresh

--- src/tokens-utils.js
+++ src/tokens-utils.js
@@ -1,7 +1,75 @@
-const tokens = require('../tokens.json');
+const { readFile, writeFile } = require('fs/promises');
+const path = require('path');
+const { SuperfaceClient } = require('@superfaceai/one-sdk');
+require('dotenv').config();
 
-function getTokens() {
-  return tokens;
+const TOKENS_FILE = path.join(__dirname, '..', 'tokens.json');
+
+const sdk = new SuperfaceClient();
+
+let _tokensCache;
+
+async function loadTokens() {
+  try {
+    const contents = await readFile(TOKENS_FILE, { encoding: 'utf-8' });
+    const parsed = JSON.parse(contents);
+    return parsed;
+  } catch (err) {
+    throw new Error('Failed to load tokens', { cause: err });
+  }
 }
 
-module.exports = { getTokens };
+async function getTokens() {
+  if (!_tokensCache) {
+    _tokensCache = await loadTokens();
+  }
+  return _tokensCache;
+}
+
+async function saveTokens(newTokens) {
+  return await writeFile(TOKENS_FILE, JSON.stringify(newTokens), {
+    encoding: 'utf-8',
+  });
+}
+
+async function getRefreshedTokens() {
+  console.error('Refreshing token');
+
+  const tokens = await getTokens();
+  const profile = await sdk.getProfile('oauth2/refresh-token');
+  const result = await profile
+    .getUseCase('GetAccessTokenFromRefreshToken')
+    .perform({
+      refreshToken: tokens.refreshToken,
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    });
+
+  const data = result.unwrap();
+  const expiresAt = Date.now() + data.expiresIn * 1000;
+
+  return { ...data, expiresAt };
+}
+
+async function refreshToken() {
+  const newTokens = await getRefreshedTokens();
+  await saveTokens(newTokens);
+  _tokensCache = newTokens;
+  return _tokensCache;
+}
+
+/**
+ * Passes current access token to the passed `perform` function and retries it with refreshed token if any exception occurs.
+ */
+async function withAccessToken(perform) {
+  const tokens = await getTokens();
+  try {
+    return await perform(tokens.accessToken);
+  } catch (err) {
+    console.error('Error, refreshing token', err);
+    const { accessToken } = refreshToken();
+    return await perform(accessToken);
+  }
+}
+
+module.exports = { withAccessToken };
