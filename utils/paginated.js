dc9cf553a895d95fd940998649737197233ec671
Jan Vlnas <616767+jnv@users.noreply.github.com>
2022-02-22 16:26:07 +0100

    fix(paginated): Condition for breaking the loop
    Co-authored-by: Jan Halama <jan.halama@superface.ai>

--- utils/paginated.js
+++ utils/paginated.js
@@ -1,6 +1,6 @@
 async function* paginated(perform, limit = Infinity) {
   let nextPage = undefined;
-  for (let i = 0; nextPage != null, i < limit; i++) {
+  for (let i = 0; nextPage != null && i < limit; i++) {
     const result = await perform(nextPage);
     if (result.isErr()) {
       throw result.error;
